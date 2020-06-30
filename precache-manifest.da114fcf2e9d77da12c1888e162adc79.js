14f220c264bf35dc938554bc194fcc56ef0dcf40
Alberto <alpagor@gmail.com>
2020-06-30 08:31:46 +0200

    Updates

---
+++ precache-manifest.da114fcf2e9d77da12c1888e162adc79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "111835c52b56b8ee2c81223ee5bf5a19",
+    "url": "/corona_app/index.html"
+  },
+  {
+    "revision": "7bd86aee95bc3055d7df",
+    "url": "/corona_app/static/css/main.2aaa066e.chunk.css"
+  },
+  {
+    "revision": "6e3606ca950552d6955c",
+    "url": "/corona_app/static/js/2.151af06a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona_app/static/js/2.151af06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd86aee95bc3055d7df",
+    "url": "/corona_app/static/js/main.415c7f90.chunk.js"
+  },
+  {
+    "revision": "eefba636a8f3bc31057d",
+    "url": "/corona_app/static/js/runtime-main.3b864a05.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona_app/static/media/image.d7265326.png"
+  }
+]);
